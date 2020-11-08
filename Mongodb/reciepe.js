8837938881d9e55192758d92a29745b0a1e7bc33
hlalsabo <hlalsabo@jpl.caltch.edu>
2020-11-07 23:30:09 -0800

    Mongdb
    add images

--- Mongodb/reciepe.js
+++ Mongodb/reciepe.js
@@ -2,148 +2,97 @@
 
 const MongoClient = require("mongodb").MongoClient;
 
-
-let recipes = [
-        {
-            name: "Mashed Potatoes",
-            Yield: 6,
-            Ingredients: 
+const Recieoiesdocs = [
+    {
+        "Name": "Mashed Potatoes",
+        "Yield": 6,
+        "Ingredients":
+            [{ Name: "golden creamer potato", quantity: "4 pound" }, { Name: "chives", quantity: "2 tablespoons chopped" }],
+        "Directions": ["add the bay leaf", "boil over medium-high heat and cook until the potatoes are tender",
+            "heat the cream and butter in a small saucepan", "Add the hot cream and season with salt "]
+    },
+    {
+        "Name": "Beef Brisket",
+        "Yield": 10,
+        "Ingredients":
             [
-              {Name:"golden creamer potato",Measurement:"4 pound"}
-            
-            ]
-        },
-        {
-            name: "Steak Potatoes",
-            Yield: 6,
-            Ingredients: 
-            [
-              {Name:"golden creamer potato",Measurement:"4 pound"}, 
-              {Name:" beef",Measurement:"8 medium"}
-            
-            ]
-        }, {
-            name: "Italian Steak",
-            Yield: 6,
-            Ingredients: 
-            [
-              {Name:"golden creamer ",Measurement:"4 pound"}, 
-              {Name:" beef",Measurement:"8 medium"}
-            
-            ]
-        },
-        {
-            name: "Steak",
-            Yield: 6,
-            Ingredients: 
-            [
-              {Name:"golden creamer potato",Measurement:"4 pound"}
-            
-            ]
-        }
+                { Name: "sprigs", quantity: "4 sprigs fresh rosemary" }, { Name: "golden creamer potato", quantity: "1 pound" },
+                { Name: " beef brisket", quantity: "1 (4 pound)" }, { Name: "onions", quantity: "4 large red onions, halved" }
+            ],
+        "Directions": ["Preheat the oven to 325 degrees F", " Put the garlic-rosemary paste in a small bowl and add 2 tablespoons of olive oil",
+            "Put the brisket in the roasting pan and sear to form a nice brown crust on both sides", "ay the vegetables all around the brisket and pour the rosemary paste"]
 
-    ]
-// let recipes = [
-//     {
-//         name: "Mashed Potatoes",
-//         Yield: 6,
-//         Ingredients: 
-//         [
-//           {Name:"golden creamer potatoes",Measurement:"4 pound"}, 
-//           {Name:" Roma tomatoes",Measurement:"8 medium"}
-        
-//         ],
-//         Directions: ["mash the garlic and 1/2 teaspoon of the salt", "toss in the parsley and bay leaves. Cover the pan tightly with aluminum foil and transfer to the oven. Bake for about 3 to 4 hours, basting every 30 minutes with the pan juices, until the beef is fork tender.", "Remove the brisket to a cutting board and let it rest for 15 minutes. Scoop the vegetables out of the roasting pan and onto a platter, cover to keep warm."]
-//     },
-//     {
-//         name: "Beef Kebabs",
-//         Yield: 4,
-//         Ingredients: ["2 pounds sirloin steak", "1 medium white onion, roughly chopped", "8 medium Roma tomatoes"],
-//         Directions: ["Trim any excess fat from the steaks, cut into 1-inch cubes, and place in a large nonreactive bow", "Place onion, garlic, lime juice, salt, pepper, and saffron or cumin, in a food processor and process until smooth. Pour mixture over meat, toss to coat, cover and marinate 30 minutes at room temperature, or up to 12 hours in the refrigerator", "heat grill to medium heat (350 degrees F) and oil well. Thread meat on skewers, leaving at least 1/2-inch space between each piece of meat. In a small bowl and the tomatoes, drizzle with oil, season with salt and pepper, and toss to coat thoroughly. Place meat and tomatoes on grill. Cook meat, turning once, until charred and medium-rare, about 7 to 10 minutes"]
-//     }, {
-//         name: "Beef Steak",
-//         Yield: 10,
-//         Ingredients: ["4 large garlic cloves, smashed", "1 (4 pound) beef brisket, first-cut", "4 large carrots, cut in 3-inch chunks", "4 large red onions, halved", "1 (16-ounce) can whole tomatoes, hand-crushed", "4 pounds golden creamer potato"],
-//         Directions: ["mash the garlic and 1/2 teaspoon of the salt", "toss in the parsley and bay leaves. Cover the pan tightly with aluminum foil and transfer to the oven. Bake for about 3 to 4 hours, basting every 30 minutes with the pan juices, until the beef is fork tender.", "Remove the brisket to a cutting board and let it rest for 15 minutes. Scoop the vegetables out of the roasting pan and onto a platter, cover to keep warm."]
+    },
+    {
+        "Name": "Grilled Flank Steak",
+        "Yield": 6,
+        "Ingredients":
+            [
+                { Name: " flank steak", quantity: "2 pounds" }, { Name: "rosemary", quantity: "1 teaspoon chopped" }, { Name: "oregano", quantity: "1 teaspoon chopped" }
+            ],
+        "Directions": ["Make the marinade: Combine the canola oil, vinegar, mustard, garlic, oregano, parsley, rosemary and Worcestershire sauce in a food processor", "Make the marinade: Combine the canola oil, vinegar, mustard, garlic, oregano, parsley, rosemary and Worcestershire sauce in a food processor", "Bring the marinade to a boil, then reduce the heat to a simmer and cook until thickened, 2 to 3 minutes.",
+            "Preheat a grill to high and oil the grates. Grill the steak 3 to 5 minutes per side for medium rare"]
+    }
+]
 
-//     },
-//     {
-//         name: "Steak",
-//         Yield: 1,
-//         Ingredients: ["4 large garlic cloves, smashed", "1 (4 pound) beef brisket, first-cut", "4 large carrots, cut in 3-inch chunks", "4 large red onions, halved", "1 (16-ounce) can whole tomatoes, hand-crushed"],
-//         Directions: ["mash the garlic and 1/2 teaspoon of the salt", "toss in the parsley and bay leaves. Cover the pan tightly with aluminum foil and transfer to the oven. Bake for about 3 to 4 hours, basting every 30 minutes with the pan juices, until the beef is fork tender.", "Remove the brisket to a cutting board and let it rest for 15 minutes. Scoop the vegetables out of the roasting pan and onto a platter, cover to keep warm."]
 
-//     }
-// ];
+
 
 let dbUsername = "cs5220stu01";
 let dbPassword = "d1vhqzJfb6Ed";
 let dbUrl = `mongodb://${dbUsername}:${dbPassword}@cs3.calstatela.edu:4042/${dbUsername}`;
 let db;
 let client;
-async function drop() {
+
+/* Connect to MongoDB Server*/
+async function ConnectDB() {
     // connect to mongodb 
     client = await MongoClient.connect(dbUrl, { useUnifiedTopology: true });
-    // selece a DB
+    // Switch to DB
     db = client.db("cs5220stu01");
-    //drop the recipes collection
-    await db.collection("recipes").drop(function (err, delOK) {
-        if (err) throw err;
-        if (delOK) console.log("Collection deleted");
-    });
+    console.log("Connected correctly to server");
+
 }
 
 async function run() {
 
     let collection = await db.collection("recipes");
-    await collection.insertMany(recipes);
-    // create a text index on the name field
-    //
-    //  \"beef potato  potato $text: { $search: '"beef" "potato"' }  
-    //{"$text": { "$search": "beef , potato" }
+    collection.insertMany(Recieoiesdocs);
 
-    //1)await collection.find( {"$text": { "$search": "beef ,potato$" }})
+    await collection.createIndex({ name: "text", "Ingredients.Name": "text" });
 
+    const cursor = await collection.find({ "$text": { "$search": ' "beef"  "potato" ' } }, { _id: 1, name: 1 })
+    cursor.count(function (err, count) {
+        console.log("Successfully found document (recipes) that use the ingredients 'beef' and 'potato' # " + count);
+    });
+    await cursor.forEach(function (myDoc) {
 
-    await collection.createIndex({ name: "text","Ingredients.Name":"text"} );
-    const cursor = await  collection.find({"$text": { "$search": ' "beef"  "potato" '}},{_id:1,name:1})
-        cursor.count(function(err, count){
-            console.log("Total matches: "+count);
-        });
-
-        await cursor.forEach(function (myDoc) {
-           
-                console.log("id    " + myDoc._id + "  Name:  " + myDoc.name);
-       
-        })
-
-
-
-     const cursor2 = await collection.find({ $text: { $search: "Steak"}},{_id:true,name:1});
-     await cursor2.forEach(function (myDoc) {   
-                console.log("id    " + myDoc._id + "  Name:  " + myDoc.name);
-
+        console.log("id    " + myDoc._id + "  Name:  " + myDoc.Name);
     })
 
-    await client.close();
+    const cursor2 = await collection.find({ $text: { $search: "Steak" } }, { _id: true, name: 1 });
+    cursor2.count(function (err, count) {
+        console.log("Successfully found document (recipes) whose names include the word 'Steak' # " + count);
+    });
+    await cursor2.forEach(function (myDoc) {
+        console.log("id    " + myDoc._id + "  Name:  " + myDoc.Name);
+    })
+
+
+
+    client.close();
 }
-
-
-
 // main method that drop first then add collection
 async function init() {
 
-    await drop()
+    await ConnectDB()
     await run();
+
+
+
+
 }
 
 // call the init method 
 init()
-// await cursor.forEach(function (myDoc) {
-    //     // myDoc.forEach(function (item) {
-    //     //     if (item.includes("beef")) {
-    //     //         //Print out the id and name of the recipes found.
-    //            console.log("myDoc    " + myDoc );
-    //     //     }
-    //     // })
-    // })
+
