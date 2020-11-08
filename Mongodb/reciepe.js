f8c1a1d54752d92a2cdf2ecec758f981d0f3020e
Sabonchi <AL_SABONCHI_HASANAIN@student.smc.edu>
2020-11-07 23:32:59 -0800

    Update reciepe.js

--- Mongodb/reciepe.js
+++ Mongodb/reciepe.js
@@ -1,3 +1,17 @@
+ /* Arecipe, like General Tso's Cauliflower (Links to an external site.), 
+ has a name, yield (i.e. number of servings), a number of ingredients, 
+ and directions (which consist of a number of steps). 
+ Note that quantity needs to be specified for some ingredients (e.g. 2 teaspoons of oil) but not for the others (e.g. salt).
+please create a Node.js program that does the following:
+
+Create two recipes objects. You may hard-code a couple of simple, make-up recipes 
+-- they don't need to be complex or real, but they should have all the properties described above.
+Save them to your MongoDB database on CS3.
+Query the database for recipes that use the ingredients "beef" and "potato". Print out the id and name of the recipes found.
+Query the database for recipes whose names include the word "Steak". 
+Print out the id and name of the recipes found. You must use Text Search for this query. You can create the necessary text index outside the program.*/
+
+
 "use strict";
 
 const MongoClient = require("mongodb").MongoClient;
